b07f9284688dad75b67a1534b836c40d9af9e691
Laci <laszlo.hajdu90@gmail.com>
2025-08-25 19:51:31 +0200

    Change base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-const repoName = '/denominate/';
+const repoName = '/';
 
 export default defineConfig({
     plugins: [react(), svgr()],
-    base: `/${repoName}/`,
+    base: `${repoName}`,
 });
 
